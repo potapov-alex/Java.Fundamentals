5b26a61e01490671480b4b01146f4a73e708f359
Aleksei Potapov <potapov.alex@hotmail.com>
2020-08-02 21:29:30 +0300

    revers args

--- MainTask02.java
+++ MainTask02.java
@@ -8,7 +8,7 @@ package JavaFundamentals;
 public class MainTask02 {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < args.length; i++)
-			System.out.println("Aрг-> " + args[i]);
+		for( int i = args.length - 1 ; i >= 0 ; i--)
+			System.out.println("Args-> " + args[i]);
 	}
 }
